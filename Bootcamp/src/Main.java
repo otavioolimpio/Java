ceb5022b98b1e535a707d8bd126732fb032d4b7c
Otávio Olímpio <otavioolimpio@outlook.com>
2026-01-16 16:35:24 -0300

    Update exercicio

--- Bootcamp/src/Main.java
+++ Bootcamp/src/Main.java
@@ -1,12 +1,16 @@
+import java.time.OffsetDateTime;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        var data_atual = OffsetDateTime.now().getYear();
         var scanner = new Scanner(System.in);
-        System.out.println("Quantos anos você tem?");
-        var age = scanner.nextInt();
-        var canDrive = (age >= 18);
-        System.out.printf("Você pode dirigir? %s \n", canDrive);
+        System.out.println("Informe seu nome:");
+        var nome = scanner.next();
+        System.out.println("Informe seu ano de nascimento");
+        var ano = scanner.nextInt();
+        var idade = data_atual - ano;
+        System.out.printf("Olá, %s, você tem %s anos.", nome, idade);
     }
 }
