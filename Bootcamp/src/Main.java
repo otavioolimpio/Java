15f18fe0e935160a20fd0ef6160d15b13658df97
Otávio Olímpio <otavioolimpio@outlook.com>
2026-01-16 15:34:13 -0300

    Repositorio do Bootcamp

---
+++ Bootcamp/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.println("Quantos anos você tem?");
+        var age = scanner.nextInt();
+        var canDrive = (age >= 18);
+        System.out.printf("Você pode dirigir? %s \n", canDrive);
+    }
+}
